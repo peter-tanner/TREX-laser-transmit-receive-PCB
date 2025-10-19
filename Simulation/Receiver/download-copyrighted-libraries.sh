838c57875f5ee90729aa3eab099cf328007c7ac6
Peter Tanner <git@petertanner.dev>
2025-10-19 18:14:44 +0800

    Add receiver reverse-bias simulation

--- Simulation/Receiver/download-copyrighted-libraries.sh
+++ Simulation/Receiver/download-copyrighted-libraries.sh
@@ -1,3 +1,4 @@
-mkdir -p temp && cd temp && curl https://www.ti.com/lit/zip/sbomc58 -OL && unzip sbomc58 && cp lmh34400.lib .. && cd .. && rm -rf temp
-mkdir -p temp && cd temp && curl https://www.ti.com/lit/zip/sbombz3 -OL && unzip sbombz3 && cp TLV3601.lib .. && cd .. && rm -rf temp
+mkdir -p temp && cd temp && curl https://www.ti.com/lit/zip/sbomc58 -OL && unzip sbomc58 && rm sbomc58 && cp lmh34400.lib .. && cd .. && rm -rf temp
+mkdir -p temp && cd temp && curl https://www.ti.com/lit/zip/sbombz3 -OL && unzip sbombz3 && rm sbombz3 && cp TLV3601.lib .. && cd .. && rm -rf temp
+mkdir -p temp && curl https://www.ti.com/lit/zip/slim251 -OL && unzip slim251 -d temp && rm slim251 && mv -f $(find temp -name '*.lib') . && rm -rf temp  
 curl https://www.ti.com/lit/zip/scem637 -OL && unzip scem637; rm scem637
